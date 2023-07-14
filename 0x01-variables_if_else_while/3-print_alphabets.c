b965b3b8226acd6bc8888efd06596eaee157003c
im2023 <alximamkoutabi@gmail.com>
2023-07-14 12:39:56 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ * description: alphABET
+ *
+ * Return: 0 (secces)
+*/
+
+int main(void)
+{
+	char ch = "a";
+	char CH = "A";
+
+	while (ch < "z")
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (CH > "Z")
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+
+	return (0);
+}
