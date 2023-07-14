d349db9aa787b287b8077c1fdac646168e5e7efd
im2023 <alximamkoutabi@gmail.com>
2023-07-13 17:58:05 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: Writing a C program that prints with printf fonction
+ *
+ * Return: always 0
+*/
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
