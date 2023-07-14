7d48e0e5f29929ced425e825b979e3da41f9c8e7
im2023 <alximamkoutabi@gmail.com>
2023-07-14 12:46:37 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,16 +13,16 @@
 
 int main(void)
 {
-	char ch = "a";
-	char CH = "A";
+	char ch = 'a';
+	char CH = 'A';
 
-	while (ch < "z")
+	while (ch < 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
 
-	while (CH < "Z")
+	while (CH < 'Z')
 	{
 		putchar(CH);
 		CH++;
