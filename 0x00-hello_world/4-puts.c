8041a04ef46ccf41c8168d8f38380adb17bb09ab
im2023 <alximamkoutabi@gmail.com>
2023-07-13 17:40:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C programme that print with puts fonction
+ *
+ * Return: Always 0 (Succes)
+*/
+
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
