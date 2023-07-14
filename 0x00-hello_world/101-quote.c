c6bf4b6acbd76e1cda0b06edb46c96c8d6accb45
im2023 <alximamkoutabi@gmail.com>
2023-07-13 20:11:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,7 @@
 */
 
 int main(void)
-
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\";
 
 	write(1,quo,59);
 
