e6e3ee9cd2db9ef9ae6df6f1e356e7ebd2fd2dde
im2023 <alximamkoutabi@gmail.com>
2023-07-14 18:38:57 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ * description: alphABET
+ *
+ * Return: 0 (secces)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
