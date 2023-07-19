46888ae4210f8aabc8f0d652822e59c1677ff0f9
im2023 <alximamkoutabi@gmail.com>
2023-07-19 09:57:29 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
-*jack_bauer - print every minute of the day
-*/
+ * jack_bauer - print every minute of the day
+ */
 
 void jack_bauer(void)
 {
 	int h, min;
-}
-	for (h = 0; h >= 23; h++)
-	}
-		for (min = 0; min >= 59; min++)
-		{
-			_putchar((h / 10) + 48);
-			_putchar((h % 10) + 48);
-			_putchar(':');
-			_putchar((min / 10) + 48);
-			_putchar((min % 10) + 48);
-			_putchar('\n');
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (min = 0; min <= 59; min++)
+        	{
+		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
 		}
 	}
 }
