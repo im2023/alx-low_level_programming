6fc4f2cd06d20a4195804408eb39e5d20b73c1be
im2023 <alximamkoutabi@gmail.com>
2023-07-19 08:12:48 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,10 @@
  *Return: the value of last_digit
 */
 
-int print_last_digit(int n, int last_digit)
+int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
 		last_digit = -1 * (n % 10);
 	else
