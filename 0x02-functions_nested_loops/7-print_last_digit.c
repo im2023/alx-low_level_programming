046edd071982b916834cf1de71d7bec522bdf805
im2023 <alximamkoutabi@gmail.com>
2023-07-19 07:57:41 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints 10 times the alphabet
+ *
+ * @n: intiger number
+ *
+ *@last_digit:  value of the last digit
+ *
+ *Return: the value of last_digit
+*/
+
+int print_last_digit(int n, last_digit)
+{
+	if (n < 0)
+		last_digit = -1 * (n % 10);
+	else
+		last_digit = n % 10;
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
