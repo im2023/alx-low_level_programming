84b76100a4299538739a1c2bd8e694e105d54d86
im2023 <alximamkoutabi@gmail.com>
2023-07-19 07:06:21 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,6 @@
 
 int print_sign(int n)
 {
-
 	if (n > 0)
 	{
 		_putchar(43);
@@ -21,7 +20,7 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	else (n < 0)
+	else
 	{
 		_putchar(45);
 		return (-1);
