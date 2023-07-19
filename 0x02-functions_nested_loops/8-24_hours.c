ff9ad2cfa4933089084fd04bd904dfc748f5c146
im2023 <alximamkoutabi@gmail.com>
2023-07-19 09:58:43 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ void jack_bauer(void)
 	for (h = 0; h <= 23; h++)
 	{
 		for (min = 0; min <= 59; min++)
-        	{
+		{
 		_putchar((h / 10) + '0');
 		_putchar((h % 10) + '0');
 		_putchar(':');
