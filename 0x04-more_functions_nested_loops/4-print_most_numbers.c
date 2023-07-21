17ee5918c18e02732d8053e20abbca6685dcfe32
im2023 <alximamkoutabi@gmail.com>
2023-07-20 18:41:59 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,8 @@
 
 /**
  * print_most_numbers - unction that prints the numbers,
- *     from 0 to 9, followed by a new line
+ *     from 0 to 9, followed by a new line exept 2 and 4.
+ * num: numbers
  *
 */
 
