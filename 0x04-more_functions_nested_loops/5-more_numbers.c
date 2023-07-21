28b7d3f74216d5222279212320abf851b73781c1
im2023 <alximamkoutabi@gmail.com>
2023-07-21 07:51:19 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,14 @@
 
 void more_numbers(void)
 {
-	int num, row, count;
+	int num , row, count;
 
-	for (row = 0; row <= 10; row++)
+	for (row = 1; row <= 10; row++)
 	{
 		for (count = 0; count <= 14; count++)
 		{
 			num = count;
-			if (count >= 10)
+			if (count > 9)
 			{
 				_putchar(1 + 48);
 				num = count % 10;
