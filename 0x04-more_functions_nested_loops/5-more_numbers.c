ed722b81246647fa061be967dcd016e2327d2e2b
im2023 <alximamkoutabi@gmail.com>
2023-07-20 19:13:33 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,8 +3,11 @@
 /**
  * more_numbers - function that prints 10 times the numbers
  *      from 0 to 14, followed by a new line
+ *num: thirt output
+ *count: second output
+ *row: first output
  *
- * Return : always 0 (succes)
+ * Return: Always 0 (succes)
 */
 
 void more_numbers(void)
