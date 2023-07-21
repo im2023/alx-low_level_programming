780c7e64fdaea2d9135653cfe38232acbbaaeab4
im2023 <alximamkoutabi@gmail.com>
2023-07-21 08:09:51 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,18 +12,14 @@ void print_diagonal(int n)
 	int postn, space;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		for (postn = 1; postn <= n; postn++)
 		{
 			for (space = 1; space <= postn; space++)
-			{
 				_putchar(' ');
-			}
-			_putchar('\\');
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
