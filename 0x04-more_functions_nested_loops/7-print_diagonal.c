86d619c8fb5fba378f0d647d3d10cbece7929a81
im2023 <alximamkoutabi@gmail.com>
2023-07-20 19:39:48 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,9 +3,28 @@
 /**
  * print_diagonal - prints 10 times the alphabet
  *
+ *@n: is the numer of time the character \ is
+ *    should be print
 */
 
 void print_diagonal(int n)
 {
-	
+	int postn, space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (space = 1; space <= postn; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			-putchar('\n');
+		}
+	}
 }
