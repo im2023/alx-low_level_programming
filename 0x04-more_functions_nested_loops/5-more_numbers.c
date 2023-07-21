0a77a142691c75e0ba0677e4a71e68b2bb76b51f
im2023 <alximamkoutabi@gmail.com>
2023-07-20 19:59:39 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - function that prints 10 times the numbers
- *      from 0 to 14, followed by a new line
- *num: thirt output
- *count: second output
- *row: first output
+ * more_numbers - prints 10 times the numbers
+ *      from 0 - 14, followed by a new line
  *
  * Return: Always 0 (succes)
 */
