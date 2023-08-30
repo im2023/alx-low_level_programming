9f3d6e88e2fe3e62980c482b572bedd774c31ef3
im2023 <alximamkoutabi@gmail.com>
2023-08-29 19:22:56 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,32 +1,22 @@
 #include "lists.h"
 
 /**
- * print_listint - Puts on a show featuring elements of a linked list.
- * @h: The cast of characters (linked list) to dazzle the audience.
+ * print_listint - it's a function that prints elements of a listint_t
  *
- * Return: The number of actors on stage (number of node)
+ * @h:linked list of type listint_t
+ *
+ * Return:number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;  /* Keeping track of the superstar count */
+	size_t number = 0;
 
 	while (h)
 	{
-		/* Presenting the next superstar in the lineup */
-		printf("And now, direct from the list stage, give it up for: %d!\n", h->n);
-
-		num++;  /* Another standing ovation for a successful performance */
-
+		printf("%d\n", h->n);
+		number++;
 		h = h->next;
-
-		/* Time for a dramatic pause before the next act */
-		printf("Ladies and gentlemen, hold your applause...\n");
-		sleep(1);  /* Adding suspense for effect */
 	}
 
-	/* Applauding the collective talent on display */
-	printf("Total superstars showcased: %lu\n", num);
-	printf("Disclaimer: No egos were inflated during this show.\n");
-
-	return (num);
+	return (number);
 }
