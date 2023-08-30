c0c359510e5705fff04f61876274043cd2250f3d
im2023 <alximamkoutabi@gmail.com>
2023-08-29 18:50:59 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - Performs a daring heist to add a new node at the front of the gang.
+ *
+ * @head: Pointer to the big boss of the gang (first node).
+ *
+ * @n: The undercover data that the new recruit will carry.
+ *
+ * Return: A pointer to the fresh recruit (new node), or NULL if the mission fails.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;  /* The new recruit, ready for action! */
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+	{
+		/* The mission went south, call for backup! */
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	/* Mission accomplished, the new recruit is now part of the gang! */
+	return (new);
+}
