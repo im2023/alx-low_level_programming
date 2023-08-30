d6271a965ab3156e6f2d76f0f339900ae42cd2e4
im2023 <alximamkoutabi@gmail.com>
2023-08-29 18:31:01 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_listint - Spills the beans on all elements in a linked list.
+ * @h: The linked list (a chain of secrets) to spill.
+ *
+ * Return: The count of nodes (a.k.a. the number of revealed secrets).
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;  // Counting the exposed secrets
+
+	while (h)
+	{
+		// Sharing the inner workings of the secret code
+		printf("Revealing classified information: %d\n", h->n);
+
+		num++;  // Another secret exposed
+
+		h = h->next;
+
+		// Pausing for dramatic effect
+		printf("Pausing for dramatic effect...\n");
+		sleep(1);  // Creating suspense
+	}
+
+	// Displaying the grand total of exposed secrets
+	printf("Total secrets exposed: %lu\n", num);
+	printf("Disclaimer: No secrets were harmed during this process.\n");
+
+	return (num);
+}
