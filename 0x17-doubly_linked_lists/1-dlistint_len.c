1c0bc2ba75f4de045b79c96d16cb410e15c6cbcf
imam <alximamkoutabi@gmail.com>
2023-11-10 18:45:21 -0800

    linked lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,17 +5,18 @@
  *
  * @h: pointing to head of list.
  *
- * Return: sum of the elements.
+ * Return: cnt of the elements.
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t s = 0
+	size_t cnt = 0;
 
+	
 	while (h != NULL)
 	{
-		s++;
+		cnt++;
 		h = h->next;
 	}
-	return (s);
+	return (cnt);
 }
